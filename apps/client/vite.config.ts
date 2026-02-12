c86c4fa3e16726d29685522be3e3983a93a3d9d2
domfromasquared <dom@asquaredpro.com>
2026-02-11 22:25:23 -0800

    Update vite.config.ts

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/pocketparlour/",
   plugins: [react()],
   server: {
     port: Number(process.env.VITE_DEV_PORT ?? 5173),
